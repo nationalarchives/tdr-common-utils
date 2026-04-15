fc83ea631b2683a39ab03ce886b5375ab8b14b26
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-04-15 09:13:16 +0100

    add mapping

--- statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/MetadataReviewLogAction.scala
+++ statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/MetadataReviewLogAction.scala
@@ -1,24 +1,31 @@
 package uk.gov.nationalarchives.tdr.common.utils.statuses
 
+import uk.gov.nationalarchives.tdr.common.utils.statuses.MetadataReviewStatus._
+
 object MetadataReviewLogAction {
   sealed trait MetadataReviewLogAction {
     val value: String
+    val reviewStatus: MetadataReviewStatus
   }
 
   case object Submission extends MetadataReviewLogAction {
     val value: String = "Submission"
+    val reviewStatus: MetadataReviewStatus = Requested
   }
 
   case object Rejection extends MetadataReviewLogAction {
     val value: String = "Rejection"
+    val reviewStatus: MetadataReviewStatus = Rejected
   }
 
   case object Approval extends MetadataReviewLogAction {
     val value: String = "Approval"
+    val reviewStatus: MetadataReviewStatus = Approved
   }
 
   case object Confirmation extends MetadataReviewLogAction {
     val value: String = "Confirmation"
+    val reviewStatus: MetadataReviewStatus = Completed
   }
 
   object MetadataReviewLogAction {
