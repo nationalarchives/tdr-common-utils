eecd853808d2bfffce5a9287bcd6d559be076d6e
ian-hoyle <ian.hoyle.tna@gmail.com>
2026-04-15 11:03:32 +0100

    Add metadata review status and log actions

---
+++ statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/MetadataReviewStatus.scala
@@ -0,0 +1,33 @@
+package uk.gov.nationalarchives.tdr.common.utils.statuses
+
+object MetadataReviewStatus {
+  sealed trait MetadataReviewStatus {
+    val value: String
+  }
+
+  case object Requested extends MetadataReviewStatus {
+    val value: String = "Requested"
+  }
+
+  case object Rejected extends MetadataReviewStatus {
+    val value: String = "Rejected"
+  }
+
+  case object Approved extends MetadataReviewStatus {
+    val value: String = "Approved"
+  }
+
+  case object Completed extends MetadataReviewStatus {
+    val value: String = "Completed"
+  }
+
+  object MetadataReviewStatus {
+    def apply(s: String): MetadataReviewStatus = s match {
+      case Requested.value => Requested
+      case Rejected.value  => Rejected
+      case Approved.value  => Approved
+      case Completed.value => Completed
+      case _               => throw new RuntimeException(s"Invalid metadata review status: $s")
+    }
+  }
+}
