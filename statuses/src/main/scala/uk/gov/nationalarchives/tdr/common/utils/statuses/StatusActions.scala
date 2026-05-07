0173e0fdff758561bc0ff8a0ed32991ef2fa984f
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2026-05-07 11:16:24 +0100

    Unidentified

--- statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActions.scala
+++ statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActions.scala
@@ -4,20 +4,6 @@ import uk.gov.nationalarchives.tdr.common.utils.statuses.StatusTypes._
 import uk.gov.nationalarchives.tdr.common.utils.statuses.StatusValues._
 
 object StatusActions {
-  sealed trait StatusActionType {
-    val value: String
-  }
-
-  case object UserFixable extends StatusActionType {
-    val value: String = "UserFixable"
-  }
-
-  case object TNASupport extends StatusActionType {
-    val value: String = "TNASupport"
-  }
-
-  case class StatusAction(actionType: StatusActionType, messageKey: String)
-
   def action(statusType: StatusType, reason: StatusValue): Option[StatusAction] =
     (statusType, reason) match {
       case (_, SuccessValue)    => None
@@ -28,16 +14,15 @@ object StatusActions {
       case (FFIDType, ZeroByteFileValue)      => Some(StatusAction(UserFixable, "ffid.zeroByteFile"))
       case (FFIDType, MultipleFormatsValue)   => Some(StatusAction(UserFixable, "ffid.multipleFormats"))
       case (FFIDType, FailedValue)            => Some(StatusAction(UserFixable, "ffid.failed"))
-      case (FFIDType, CustomValue(reason))    => Some(StatusAction(UserFixable, s"ffid.$reason"))
+      case (FFIDType, CustomValue("Unidentified")) => Some(StatusAction(TNASupport,"ffid.unidentified"))
+      case (FFIDType, CustomValue(reason))          => Some(StatusAction(UserFixable, s"ffid.$reason"))
 
       case (AntivirusType, VirusDetectedValue) => Some(StatusAction(UserFixable, "antivirus.virusDetected"))
       case (AntivirusType, FailedValue)        => Some(StatusAction(UserFixable, "antivirus.failed"))
-
-      case (ChecksumMatchType, MismatchValue) => Some(StatusAction(UserFixable, "checksumMatch.mismatch"))
-      case (ChecksumMatchType, FailedValue)   => Some(StatusAction(UserFixable, "checksumMatch.failed"))
-
-      case (ClientChecksumType, FailedValue)  => Some(StatusAction(UserFixable, "clientChecksum.failed"))
-      case (ClientFilePathType, FailedValue)  => Some(StatusAction(UserFixable, "clientFilePath.failed"))
+      case (ChecksumMatchType, MismatchValue)  => Some(StatusAction(UserFixable, "checksumMatch.mismatch"))
+      case (ChecksumMatchType, FailedValue)    => Some(StatusAction(UserFixable, "checksumMatch.failed"))
+      case (ClientChecksumType, FailedValue)   => Some(StatusAction(UserFixable, "clientChecksum.failed"))
+      case (ClientFilePathType, FailedValue)   => Some(StatusAction(UserFixable, "clientFilePath.failed"))
 
       case (RedactionType, SuccessValue)               => None
       case (RedactionType, NoOriginalFileValue)        => Some(StatusAction(UserFixable, "redaction.noOriginalFile"))
@@ -47,4 +32,18 @@ object StatusActions {
 
       case (statusType, reason) => Some(StatusAction(UserFixable, s"${statusType.id.toLowerCase}.${reason.value.toLowerCase}"))
     }
+
+  sealed trait StatusActionType {
+    val value: String
+  }
+
+  case class StatusAction(actionType: StatusActionType, messageKey: String)
+
+  case object UserFixable extends StatusActionType {
+    val value: String = "UserFixable"
+  }
+
+  case object TNASupport extends StatusActionType {
+    val value: String = "TNASupport"
+  }
 }
