9a73db589c6d57ab56699dc9b3389b4858dc505f
ihoyle <ian.hoyle@nationalarchives.gov.uk>
2026-06-02 10:44:19 +0100

    correct actions to requirements

--- statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActions.scala
+++ statuses/src/main/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActions.scala
@@ -6,31 +6,38 @@ import uk.gov.nationalarchives.tdr.common.utils.statuses.StatusValues._
 object StatusActions {
   def action(statusType: StatusType, reason: StatusValue): Option[StatusAction] =
     (statusType, reason) match {
-      case (_, SuccessValue)    => None
-      case (_, CompletedValue)  => None
+      case (_, SuccessValue) => None
+      case (_, CompletedValue) => None
       case (_, InProgressValue) => None
 
       case (FFIDType, NonJudgmentFormatValue) => Some(StatusAction(UserFixable, "ffid.nonJudgmentFormat"))
-      case (FFIDType, ZeroByteFileValue)      => Some(StatusAction(UserFixable, "ffid.zeroByteFile"))
-      case (FFIDType, MultipleFormatsValue)   => Some(StatusAction(UserFixable, "ffid.multipleFormats"))
-      case (FFIDType, FailedValue)            => Some(StatusAction(UserFixable, "ffid.failed"))
-      case (FFIDType, CustomValue("Unidentified")) => Some(StatusAction(TNASupport,"ffid.unidentified"))
-      case (FFIDType, CustomValue(reason))          => Some(StatusAction(UserFixable, s"ffid.$reason"))
+      case (FFIDType, ZeroByteFileValue) => Some(StatusAction(UserFixable, "ffid.zeroByteFile"))
+      case (FFIDType, MultipleFormatsValue) => Some(StatusAction(TNASupport, "ffid.multipleFormats"))
+      case (FFIDType, FailedValue) => Some(StatusAction(TNASupport, "ffid.failed"))
+      // Custom values come fro disallowed Puids in da-metadata-schema
+      case (FFIDType, CustomValue("Unidentified")) => Some(StatusAction(TNASupport, "ffid.unidentified"))
+      case (FFIDType, CustomValue("Zip")) => Some(StatusAction(UserFixable, s"ffid.zip"))
+      case (FFIDType, CustomValue("PasswordProtected")) => Some(StatusAction(UserFixable, s"ffid.passwordProtected"))
+      case (FFIDType, CustomValue("Executable")) => Some(StatusAction(UserFixable, s"ffid.executable"))
+      case (FFIDType, CustomValue("Template")) => Some(StatusAction(UserFixable, s"ffid.template"))
+      case (FFIDType, CustomValue("Shortcut")) => Some(StatusAction(UserFixable, s"ffid.shortcut"))
+      case (FFIDType, CustomValue("OperatingSystemMac")) => Some(StatusAction(UserFixable, s"ffid.operatingSystemMac"))
+      case (FFIDType, CustomValue(reason)) => Some(StatusAction(TNASupport, s"ffid.$reason"))
 
-      case (AntivirusType, VirusDetectedValue) => Some(StatusAction(UserFixable, "antivirus.virusDetected"))
-      case (AntivirusType, FailedValue)        => Some(StatusAction(UserFixable, "antivirus.failed"))
-      case (ChecksumMatchType, MismatchValue)  => Some(StatusAction(UserFixable, "checksumMatch.mismatch"))
-      case (ChecksumMatchType, FailedValue)    => Some(StatusAction(UserFixable, "checksumMatch.failed"))
-      case (ClientChecksumType, FailedValue)   => Some(StatusAction(UserFixable, "clientChecksum.failed"))
-      case (ClientFilePathType, FailedValue)   => Some(StatusAction(UserFixable, "clientFilePath.failed"))
+      case (AntivirusType, VirusDetectedValue) => Some(StatusAction(TNASupport, "antivirus.virusDetected"))
+      case (AntivirusType, FailedValue) => Some(StatusAction(TNASupport, "antivirus.failed"))
+      case (ChecksumMatchType, MismatchValue) => Some(StatusAction(TNASupport, "checksumMatch.mismatch"))
+      case (ChecksumMatchType, FailedValue) => Some(StatusAction(TNASupport, "checksumMatch.failed"))
+      case (ClientChecksumType, FailedValue) => Some(StatusAction(TNASupport, "clientChecksum.failed"))
+      case (ClientFilePathType, FailedValue) => Some(StatusAction(TNASupport, "clientFilePath.failed"))
 
-      case (RedactionType, SuccessValue)               => None
-      case (RedactionType, NoOriginalFileValue)        => Some(StatusAction(UserFixable, "redaction.noOriginalFile"))
+      case (RedactionType, SuccessValue) => None
+      case (RedactionType, NoOriginalFileValue) => Some(StatusAction(UserFixable, "redaction.noOriginalFile"))
       case (RedactionType, AmbiguousOriginalFileValue) => Some(StatusAction(UserFixable, "redaction.ambiguousOriginalFile"))
-      case (RedactionType, DuplicateFileNameValue)     => Some(StatusAction(UserFixable, "redaction.duplicateFileName"))
-      case (RedactionType, _)                          => Some(StatusAction(UserFixable, "redaction.failed"))
+      case (RedactionType, DuplicateFileNameValue) => Some(StatusAction(UserFixable, "redaction.duplicateFileName"))
+      case (RedactionType, _) => Some(StatusAction(TNASupport, "redaction.failed"))
 
-      case (statusType, reason) => Some(StatusAction(UserFixable, s"${statusType.id.toLowerCase}.${reason.value.toLowerCase}"))
+      case (statusType, reason) => Some(StatusAction(TNASupport, s"${statusType.id.toLowerCase}.${reason.value.toLowerCase}"))
     }
 
   sealed trait StatusActionType {
--- statuses/src/test/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActionsSpec.scala
+++ statuses/src/test/scala/uk/gov/nationalarchives/tdr/common/utils/statuses/StatusActionsSpec.scala
@@ -16,30 +16,57 @@ class StatusActionsSpec extends AnyWordSpec with MockitoSugar {
       action(ClientChecksumType, InProgressValue) shouldBe None
     }
 
-    "return UserFixable with correct message key for FFID failure reasons" in {
+    "return UserFixable with correct message key for FFID failure reasons zero byte and non Judgment " in {
       action(FFIDType, NonJudgmentFormatValue) shouldBe Some(StatusAction(UserFixable, "ffid.nonJudgmentFormat"))
       action(FFIDType, ZeroByteFileValue) shouldBe Some(StatusAction(UserFixable, "ffid.zeroByteFile"))
-      action(FFIDType, MultipleFormatsValue) shouldBe Some(StatusAction(UserFixable, "ffid.multipleFormats"))
-      action(FFIDType, FailedValue) shouldBe Some(StatusAction(UserFixable, "ffid.failed"))
     }
 
-    "use dynamic reason as message key for FFID with CustomValue" in {
-      action(FFIDType, CustomValue("PasswordProtected")) shouldBe Some(StatusAction(UserFixable, "ffid.PasswordProtected"))
+    "return TNA with correct message key for FFID failure reasons multiple formats and failure " in {
+      action(FFIDType, MultipleFormatsValue) shouldBe Some(StatusAction(TNASupport, "ffid.multipleFormats"))
+      action(FFIDType, FailedValue) shouldBe Some(StatusAction(TNASupport, "ffid.failed"))
     }
 
-    "return UserFixable with correct message key for antivirus failures" in {
-      action(AntivirusType, VirusDetectedValue) shouldBe Some(StatusAction(UserFixable, "antivirus.virusDetected"))
-      action(AntivirusType, FailedValue) shouldBe Some(StatusAction(UserFixable, "antivirus.failed"))
+    "return UserFixable when FFID PasswordProtected" in {
+      action(FFIDType, CustomValue("PasswordProtected")) shouldBe Some(StatusAction(UserFixable, "ffid.passwordProtected"))
     }
 
-    "return UserFixable with correct message key for checksum match failures" in {
-      action(ChecksumMatchType, MismatchValue) shouldBe Some(StatusAction(UserFixable, "checksumMatch.mismatch"))
-      action(ChecksumMatchType, FailedValue) shouldBe Some(StatusAction(UserFixable, "checksumMatch.failed"))
+    "return TNASupport when FFID Unidentified" in {
+      action(FFIDType, CustomValue("Unidentified")) shouldBe Some(StatusAction(TNASupport, "ffid.unidentified"))
     }
 
-    "return UserFixable with correct message key for client check failures" in {
-      action(ClientChecksumType, FailedValue) shouldBe Some(StatusAction(UserFixable, "clientChecksum.failed"))
-      action(ClientFilePathType, FailedValue) shouldBe Some(StatusAction(UserFixable, "clientFilePath.failed"))
+    "return UserFixable when FFID Executable" in {
+      action(FFIDType, CustomValue("Executable")) shouldBe Some(StatusAction(UserFixable, "ffid.executable"))
+    }
+
+    "return UserFixable when FFID OperatingSystemMac" in {
+      action(FFIDType, CustomValue("OperatingSystemMac")) shouldBe Some(StatusAction(UserFixable, "ffid.operatingSystemMac"))
+    }
+
+    "return UserFixable when FFID Shortcut" in {
+      action(FFIDType, CustomValue("Shortcut")) shouldBe Some(StatusAction(UserFixable, "ffid.shortcut"))
+    }
+
+    "return UserFixable when FFID Zip" in {
+      action(FFIDType, CustomValue("Zip")) shouldBe Some(StatusAction(UserFixable, "ffid.zip"))
+    }
+
+    "return UserFixable when FFID Template" in {
+      action(FFIDType, CustomValue("Template")) shouldBe Some(StatusAction(UserFixable, "ffid.template"))
+    }
+
+    "return TNASupport with correct message key for antivirus failures" in {
+      action(AntivirusType, VirusDetectedValue) shouldBe Some(StatusAction(TNASupport, "antivirus.virusDetected"))
+      action(AntivirusType, FailedValue) shouldBe Some(StatusAction(TNASupport, "antivirus.failed"))
+    }
+
+    "return TNASupport with correct message key for checksum match failures" in {
+      action(ChecksumMatchType, MismatchValue) shouldBe Some(StatusAction(TNASupport, "checksumMatch.mismatch"))
+      action(ChecksumMatchType, FailedValue) shouldBe Some(StatusAction(TNASupport, "checksumMatch.failed"))
+    }
+
+    "return TNASupport with correct message key for client check failures" in {
+      action(ClientChecksumType, FailedValue) shouldBe Some(StatusAction(TNASupport, "clientChecksum.failed"))
+      action(ClientFilePathType, FailedValue) shouldBe Some(StatusAction(TNASupport, "clientFilePath.failed"))
     }
 
     "return UserFixable for redaction failures with specific message keys" in {
@@ -48,18 +75,18 @@ class StatusActionsSpec extends AnyWordSpec with MockitoSugar {
       action(RedactionType, DuplicateFileNameValue) shouldBe Some(StatusAction(UserFixable, "redaction.duplicateFileName"))
     }
 
-    "return UserFixable for unknown redaction failures" in {
-      action(RedactionType, FailedValue) shouldBe Some(StatusAction(UserFixable, "redaction.failed"))
-      action(RedactionType, CompletedWithIssuesValue) shouldBe Some(StatusAction(UserFixable, "redaction.failed"))
+    "return TNASupport for unknown redaction failures" in {
+      action(RedactionType, FailedValue) shouldBe Some(StatusAction(TNASupport, "redaction.failed"))
+      action(RedactionType, CompletedWithIssuesValue) shouldBe Some(StatusAction(TNASupport, "redaction.failed"))
     }
 
     "return None for redaction success" in {
       action(RedactionType, SuccessValue) shouldBe None
     }
 
-    "generate message key for unmapped combinations" in {
+    "return TNASupport is server checksum Failed" in {
       val result = action(ServerChecksumType, FailedValue)
-      result shouldBe Some(StatusAction(UserFixable, "serverchecksum.failed"))
+      result shouldBe Some(StatusAction(TNASupport, "serverchecksum.failed"))
     }
 
     "have correct string values for action types" in {
