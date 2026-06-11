f9e0db1267cf297d57e6b428a77ba27a7cfd2fb8
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2026-06-11 14:49:43 +0100

    [TDRD-1263] Add new file checks object category

--- objectkeycontext/src/test/scala/uk/gov/nationalarchives/tdr/common/utils/objectkeycontext/ObjectCategoriesSpec.scala
+++ objectkeycontext/src/test/scala/uk/gov/nationalarchives/tdr/common/utils/objectkeycontext/ObjectCategoriesSpec.scala
@@ -3,18 +3,20 @@ package uk.gov.nationalarchives.tdr.common.utils.objectkeycontext
 import org.mockito.MockitoSugar
 import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpec
-import uk.gov.nationalarchives.tdr.common.utils.objectkeycontext.ObjectCategories.{DryRunMetadata, Metadata, Records}
+import uk.gov.nationalarchives.tdr.common.utils.objectkeycontext.ObjectCategories.{DryRunMetadata, FileChecks, Metadata, Records}
 
 class ObjectCategoriesSpec extends AnyWordSpec with MockitoSugar {
   "ObjectCategories" should {
     "have the correct field values" in {
       DryRunMetadata.id shouldEqual "dryrunmetadata"
+      FileChecks.id shouldEqual "filechecks"
       Metadata.id shouldEqual "metadata"
       Records.id shouldEqual "records"
     }
 
     "return the correct 'object categories' based on input string" in {
       ObjectCategories.toObjectCategory("dryrunmetadata") shouldBe DryRunMetadata
+      ObjectCategories.toObjectCategory("filechecks") shouldBe FileChecks
       ObjectCategories.toObjectCategory("metadata") shouldBe Metadata
       ObjectCategories.toObjectCategory("records") shouldBe Records
     }
