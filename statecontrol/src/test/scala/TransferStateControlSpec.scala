605620abc3668ee53370444ed90b4ecea30e6f2a
TomJKing <tom.king@nationalarchives.gov.uk>
2026-06-12 07:48:24 +0100

    Fix unit test names

--- statecontrol/src/test/scala/TransferStateControlSpec.scala
+++ statecontrol/src/test/scala/TransferStateControlSpec.scala
@@ -7,7 +7,7 @@ import uk.gov.nationalarchives.tdr.common.utils.statecontrol._
 import java.util.UUID
 
 class TransferStateControlSpec extends SpecUtils {
-  "'changeTransferState" should "return an exception when state change consignment id does not match current state consignment id" in {
+  "transferStateChangeValid" should "return an exception when state change consignment id does not match current state consignment id" in {
     val currentState = List(ConsignmentStatuses(UUID.randomUUID(), UUID.randomUUID(), UploadType.id, InProgressValue.value, someDateTime, None))
     val stateChange = StateChange(consignmentId, UploadType, CompletedValue)
     val result = TransferStateControl.transferStateChangeValid(stateChange, currentState)
@@ -15,7 +15,7 @@ class TransferStateControlSpec extends SpecUtils {
     result shouldBe Left(StateChangeException("Request contains mismatched consignment ids"))
   }
 
-  "'changeTransferState" should "return an exception when current state contains different consignment ids" in {
+  "transferStateChangeValid" should "return an exception when current state contains different consignment ids" in {
     val currentState = List(
       ConsignmentStatuses(UUID.randomUUID(), consignmentId, UploadType.id, InProgressValue.value, someDateTime, None),
       ConsignmentStatuses(UUID.randomUUID(), UUID.randomUUID(), UploadType.id, InProgressValue.value, someDateTime, None)
